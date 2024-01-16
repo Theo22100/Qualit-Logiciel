fe8d1375c072499f9717e41b0ec8445ac201d256
Theo22100 <theoguerinBZH@hotmail.com>
2024-01-16 15:13:18 +0100

    Rajout parser

--- back/src/app.js
+++ back/src/app.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
 const app = express(); 
-const bodyParser = require('body-parser'); 
 const session = require('express-session');
 const path = require('path');
+const bodyParser = require('body-parser'); // middleware
+
 
 const mysql = require('mysql');
 
@@ -13,45 +14,55 @@ const connection = mysql.createConnection({
   database: 'qualite'
 });
 
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
   const indexPath = path.join(__dirname, '..', '..', 'front', 'index.html');
-  res.sendFile(indexPath);
+  response.sendFile(indexPath);
 });
 
-app.post('/connexion', (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
+app.post('/connexion', (request, response) => {
+  let email = request.body.email;
+  let password = request.body.password;
   console.log(email);
   if (email && password) {
-		// Execute SQL query that'll select the account from the database based on the specified email and password
-		connection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				request.session.loggedin = true;
-				request.session.email = email;
-				// Redirect to home page
-				response.redirect('/profile');
-			} else {
-				response.send('Incorrect Email and/or Password!');
-			}			
-			response.end();
-		});
-	} else {
-		response.send('Please enter Email and Password!');
-		response.end();
-	}
+    // Execute SQL query that'll select the account from the database based on the specified email and password
+    connection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+      // Error
+      if (error) {
+        console.error(error);
+        response.status(500).send('Internal Server Error');
+        return;
+      }
+      // If the account exists
+      if (results.length > 0) {
+        // Authenticate the user
+        request.session.loggedin = true;
+        request.session.email = email;
+        // Redirect to home page
+        response.redirect('/profile');
+      } else {
+        response.send('Incorrect Email and/or Password!');
+      }
+      response.end();
+    });
+  } else {
+    response.send('Please enter Email and Password!');
+    response.end();
+  }
 });
 
 app.get('/profile', function(request, response) {
 	// If the user is loggedin
 	if (request.session.loggedin) {
 		// Output username
-		response.send('Welcome back, ' + request.session.email + '!');
+		response.send('Bienvenue, ' + request.session.email + '!');
 	} else {
 		// Not logged in
 		response.send('Please login to view this page!');
