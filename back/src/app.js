47daad020ae347fe7af8e23c28b77d9d5fa870e6
Theo22100 <theoguerinBZH@hotmail.com>
2024-01-16 11:32:45 +0100

    Ajout SQL + express session

--- back/src/app.js
+++ back/src/app.js
@@ -1,7 +1,17 @@
 const express = require('express'); 
 const app = express(); 
 const bodyParser = require('body-parser'); 
+const session = require('express-session');
 const path = require('path');
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'qualite'
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -10,14 +20,43 @@ app.get('/', (req, res) => {
   res.sendFile(indexPath);
 });
 
-
-
 app.post('/connexion', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
   console.log(email);
-  console.log(password);
-  res.send(`Email: ${email} Password: ${password}`);
+  if (email && password) {
+		// Execute SQL query that'll select the account from the database based on the specified email and password
+		connection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.email = email;
+				// Redirect to home page
+				response.redirect('/profile');
+			} else {
+				response.send('Incorrect Email and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Email and Password!');
+		response.end();
+	}
+});
+
+app.get('/profile', function(request, response) {
+	// If the user is loggedin
+	if (request.session.loggedin) {
+		// Output username
+		response.send('Welcome back, ' + request.session.email + '!');
+	} else {
+		// Not logged in
+		response.send('Please login to view this page!');
+	}
+	response.end();
 });
 
 const port = 3000 
