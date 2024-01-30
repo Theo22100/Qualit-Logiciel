97dc1cac89d0b03c07bd6a781120ce6255f77dce
Theo22100 <theoguerinBZH@hotmail.com>
2024-01-30 10:24:06 +0100

    Fix + A reparer profile.html lors de connexion

--- back/src/app.js
+++ back/src/app.js
@@ -1,5 +1,5 @@
-const express = require('express'); 
-const app = express(); 
+const express = require('express');
+const app = express();
 const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser'); // middleware
@@ -7,9 +7,9 @@ const bodyParser = require('body-parser'); // middleware
 const dbConnection = require('./database.js');
 
 app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -21,46 +21,45 @@ app.get('/', (request, response) => {
 app.post('/connexion', (request, response) => {
   let email = request.body.email;
   let password = request.body.password;
+
   if (email && password) {
-    // Execute SQL
-    dbConnection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
-      // Error
+    dbConnection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], function (error, results, fields) {
       if (error) {
         console.error(error);
         response.status(500).send('Internal Server Error');
-        return;
+        return; 
       }
-      // If the account exists
+
       if (results.length > 0) {
-        // Authenticate the user
         request.session.loggedin = true;
         request.session.email = email;
-        // Redirect to home page
         response.redirect('/profile');
       } else {
         response.send('Incorrect Email and/or Password!');
-        
+        response.end(); 
       }
-      response.end();
     });
   } else {
     response.send('Please enter Email and Password!');
-    response.end();
+    response.end(); 
   }
 });
 
-app.get('/profile', function(request, response) {
-	// If the user is loggedin
-	if (request.session.loggedin) {
-		// Output username
-		response.send('Bienvenue, ' + request.session.email + '!');
-	} else {
-		// Not logged in
-		response.send('Please login to view this page!');
-	}
-	response.end();
+
+
+
+
+
+app.get('/profile', function (request, response) {
+  if (request.session.loggedin) {
+    const indexPath2 = path.join(__dirname, '..', '..', 'front', 'profile.html');
+    response.sendFile(indexPath2);
+  } else {
+    response.send('Please login to view this page!');
+  }
+  response.end(); 
 });
 
-const port = 3000 
+const port = 3000;
 
-app.listen(port, () => console.log(`This app is listening on port ${port}`));
+app.listen(port, () => console.log(`App Port ${port}`));
